46161d7a2498c9694f4f261b49cf6bed8b9a9cbc
amannagar1809 <amannagar1809@gmail.com>
2026-05-24 04:27:16 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Company Review Application</h1>
+      <p>React setup is working successfully.</p>
+    </div>
+  );
+}
+
+export default App;
